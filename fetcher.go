0115102f103bf82f26be410bcace22881985ff7d
Anthony Alves <cvballa3g0@gmail.com>
2017-10-09 11:51:30 -0400

    add account creation..
    TODO fix encoding when sending POST requests

--- fetcher.go
+++ fetcher.go
@@ -12,6 +12,7 @@ import (
 	"net/http/cookiejar"
 	"net/url"
 	"strconv"
+	"log"
 )
 
 type blob struct {
@@ -21,10 +22,15 @@ type blob struct {
 
 type session struct {
 	id                string
+	token             string
 	keyIterationCount int
 	cookieJar         http.CookieJar
 	key               []byte
 }
+
+var (
+	LastPassBaseUrl = "https://lastpass.com"
+)
 
 var (
 	ErrInvalidPassword = fmt.Errorf("invalid username or password")
@@ -62,16 +68,19 @@ func make_session(username, password string, iterationCount int) (*session, erro
 	defer res.Body.Close()
 	var response struct {
 		SessionId string `xml:"sessionid,attr"`
+		Token     string `xml:"token,attr"`
 	}
 	err = xml.NewDecoder(res.Body).Decode(&response)
 	if err != nil {
 		return nil, err
 	}
 
+	key := makeKey(username, password, iterationCount)
 	return &session{response.SessionId,
+		response.Token,
 		iterationCount,
 		cookieJar,
-		makeKey(username, password, iterationCount),
+		key,
 	}, nil
 }
 
@@ -108,6 +117,47 @@ func fetch(s *session) (*blob, error) {
 		return nil, err
 	}
 	return &blob{b, s.keyIterationCount}, nil
+}
+
+func post(postUrl *url.URL, s *session, values *url.Values) (string, error) {
+	log.Println(postUrl.String())
+	if values == nil {
+		values = &url.Values{}
+	}
+
+	values.Set("token", string(s.token))
+	// TODO fix encoding b64
+	// 2017/10/09 11:46:30 <xmlresponse><result action="added" aid="2215972459054203220" urid="0" msg="accountadded" acctname1="" acctname2="" acctname3="" acctname4="" acctname5="" acctname6="" grouping="!dBYwP0uxf3HfGMqnRoWcMQ==|SWtDmymO8K7rB8wJWAUVoQ==" count="0" lasttouch="0000-00-00 00:00:00" editlink="" url="687474703a2f2f66616365626f6f6b2e636f6d" fav="0" launchjs="" deleted="0" remoteshare="0" username="IbMaWDJ4UpzVaOACQYsaVZ8B3U4TsvwBmwUKg1Ok0Q6eAAAAAAAAAA==" localupdate="1" accts_version="36" pwprotect="0" submit_id="" captcha_id="" custom_js="" ></result></xmlresponse>
+	client := &http.Client{
+		Jar: s.cookieJar,
+	}
+
+	res, err := client.PostForm(postUrl.String(), *values)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil && err != io.EOF {
+		return "", err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("%s", res.Status)
+	}
+
+	return string(b), nil
+}
+
+func encodeValues(values *url.Values) *url.Values {
+	newValues := &url.Values{}
+	for key, val := range *values {
+		for _, v := range val {
+			newValues.Add(key, base64.StdEncoding.EncodeToString(s2b(v)))
+		}
+	}
+	return newValues
 }
 
 func requestIterationCount(username string) (int, error) {
--- lastpass.go
+++ lastpass.go
@@ -3,6 +3,10 @@ package lastpass
 import (
 	"bytes"
 	"fmt"
+	"net/url"
+	"encoding/xml"
+	"strings"
+	"io"
 )
 
 var (
@@ -60,40 +64,97 @@ func (lp LastPass) GetAccounts() ([]*Account, error) {
 // GetAccount gets LastPass account by unique ID
 // If not found, returns ErrAccountNotFound error
 func (lp LastPass) GetAccount(id string) (*Account, error) {
-	return lp.Search(id, Id, CaseInsensitive)
+	accs, err := lp.Search(id, Id, CaseInsensitive)
+	if err != nil {
+		return nil, err
+	} else if accs == nil || len(accs) == 0 {
+		return nil, ErrAccountNotFound
+	}
+
+	return accs[0], nil
 }
 
 // Search looks for LastPass accounts matching given args.
-// Returns the first account found or ErrAccountNotFound error
-func (lp LastPass) Search(value string, field Field, method SearchMethod) (*Account, error) {
+func (lp LastPass) Search(value string, field Field, method SearchMethod) ([]*Account, error) {
 	accs, err := lp.GetAccounts()
 	if err != nil {
 		return nil, err
 	}
 
-	err = ErrAccountNotFound
-	var account *Account
+	matchedAccounts := []*Account{}
 
 	matchFunc := matchFuncs[method]
 	for _, acc := range accs {
 		if matchFunc(getValue(*acc, field), value) {
-			account = acc
-			err = nil
-			break
+			matchedAccounts = append(matchedAccounts, acc)
 		}
 	}
 
+	return matchedAccounts, nil
+}
+
+func (lp *LastPass) UpdateAccount(account *Account) (*Account, error) {
+
+	_, err := lp.upsertAccount(account)
 	return account, err
 }
 
-func (lp *LastPass) UpdateAccount(account Account) (*Account, error) {
-	return nil, fmt.Errorf("NotImplemented")
+func (lp *LastPass) CreateAccount(account *Account) (*Account, error) {
+	account.Id = "0"
+	resp, err := lp.upsertAccount(account)
+	if err != nil {
+		return nil, err
+	}
+
+	// https://github.com/gnewton/chidley
+	var response struct {
+		Result struct {
+			AttrAcctname1     string `xml:" acctname1,attr"  json:",omitempty"`
+			AttrAcctname2     string `xml:" acctname2,attr"  json:",omitempty"`
+			AttrAcctname3     string `xml:" acctname3,attr"  json:",omitempty"`
+			AttrAcctname4     string `xml:" acctname4,attr"  json:",omitempty"`
+			AttrAcctname5     string `xml:" acctname5,attr"  json:",omitempty"`
+			AttrAcctname6     string `xml:" acctname6,attr"  json:",omitempty"`
+			AttrAccts_version string `xml:" accts_version,attr"  json:",omitempty"`
+			AttrAction        string `xml:" action,attr"  json:",omitempty"`
+			AttrAid           string `xml:" aid,attr"  json:",omitempty"`
+			AttrCaptcha_id    string `xml:" captcha_id,attr"  json:",omitempty"`
+			AttrCount         string `xml:" count,attr"  json:",omitempty"`
+			AttrCustom_js     string `xml:" custom_js,attr"  json:",omitempty"`
+			AttrDeleted       string `xml:" deleted,attr"  json:",omitempty"`
+			AttrEditlink      string `xml:" editlink,attr"  json:",omitempty"`
+			AttrFav           string `xml:" fav,attr"  json:",omitempty"`
+			AttrGrouping      string `xml:" grouping,attr"  json:",omitempty"`
+			AttrLasttouch     string `xml:" lasttouch,attr"  json:",omitempty"`
+			AttrLaunchjs      string `xml:" launchjs,attr"  json:",omitempty"`
+			AttrLocalupdate   string `xml:" localupdate,attr"  json:",omitempty"`
+			AttrMsg           string `xml:" msg,attr"  json:",omitempty"`
+			AttrPwprotect     string `xml:" pwprotect,attr"  json:",omitempty"`
+			AttrRemoteshare   string `xml:" remoteshare,attr"  json:",omitempty"`
+			AttrSubmit_id     string `xml:" submit_id,attr"  json:",omitempty"`
+			AttrUrid          string `xml:" urid,attr"  json:",omitempty"`
+			AttrUrl           string `xml:" url,attr"  json:",omitempty"`
+			AttrUsername      string `xml:" username,attr"  json:",omitempty"`
+		} `xml:" result,omitempty" json:"result,omitempty"`
+	}
+
+	if err = xml.NewDecoder(strings.NewReader(resp)).Decode(&response); err != nil && err != io.EOF {
+		return nil, err
+	}
+
+	account.Id = response.Result.AttrAid
+	return account, nil
 }
 
-func (lp *LastPass) CreateAccount(account Account) error {
-	return fmt.Errorf("NotImplemented")
+func (lp *LastPass) upsertAccount(account *Account) (string, error) {
+	bUrl, err := url.Parse(LastPassBaseUrl)
+	if err != nil {
+		return "", err
+	}
+	bUrl.Path = "show_website.php"
+	return post(bUrl, lp.sesh, account.encrypt(lp.sesh.key))
 }
 
-func (lp *LastPass) Delete(account Account) error {
+func (lp *LastPass) DeleteAccount(account *Account) error {
 	return fmt.Errorf("NotImplemented")
 }
