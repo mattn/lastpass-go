2524ddaf39a8c63fedaa2cc6ee79e91b4903d1ab
Anthony Alves <aalves@fit.edu>
2017-10-10 14:42:07 -0400

    move account to own file
    Future plans is to keep this fork as version 1 of mattn's until. Then officially fork off of his repo and v2 vault will be the main struct (not LastPass)

---
+++ account.go
@@ -0,0 +1,55 @@
+package lastpass
+
+import (
+	"fmt"
+	"net/url"
+	lcrypt "github.com/while-loop/lastpass-go/internal/crypt"
+)
+
+type Account struct {
+	Id       string `json:"id"`
+	Name     string `json:"name"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Url      string `json:"url"`
+	Group    string `json:"group"`
+	Notes    string `json:"notes"`
+}
+
+func (a Account) String() string {
+	return fmt.Sprintf("Id: %s, Name: %s, Username: %s", a.Id, a.Name, a.Username)
+}
+
+func (a Account) encrypt(key []byte) *url.Values {
+	//Id       plain
+	//Name     aes & b64
+	//Username aes & b64
+	//Password aes & b64
+	//Url      hex
+	//Group    aes & b64
+	//Notes    aes & b64
+
+	vals := &url.Values{}
+
+	vals.Set("aid", a.Id)
+	vals.Set("url", b2s(lcrypt.EncodeHex(s2b(a.Url))))
+	vals.Set("username", b2s(lcrypt.EncryptAes256Cbc(s2b(a.Username), key)))
+	vals.Set("password", b2s(lcrypt.EncryptAes256Cbc(s2b(a.Password), key)))
+	vals.Set("extra", b2s(lcrypt.EncryptAes256Cbc(s2b(a.Notes), key))) // notes
+	vals.Set("name", b2s(lcrypt.EncryptAes256Cbc(s2b(a.Name), key)))
+	vals.Set("grouping", b2s(lcrypt.EncryptAes256Cbc(s2b(a.Group), key)))
+	vals.Set("pwprotect", "off") // TODO(while-loop) find out what this field does
+
+	// request info
+	vals.Set("extjs", "1")
+	vals.Set("method", "cli")
+	return vals
+}
+
+func s2b(string string) []byte {
+	return []byte(string)
+}
+
+func b2s(data []byte) string {
+	return string(data)
+}
